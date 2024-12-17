9d85ece5def4f034d19ac397aac6227c2b97cd6e
nazariikyselevych <nazarkysel@gmail.com>
2024-12-17 18:37:56 +0200

    feat: rpc env

--- example-agent/src/common/utils/envConfig.ts
+++ example-agent/src/common/utils/envConfig.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import { cleanEnv, host, num, port, str, testOnly } from "envalid";
+import { url, cleanEnv, host, num, port, str, testOnly } from "envalid";
 
 dotenv.config();
 
@@ -10,4 +10,5 @@ export const env = cleanEnv(process.env, {
   CORS_ORIGIN: str({ devDefault: testOnly("http://localhost:3000") }),
   COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
   COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
+  RPC_URL: url(),
 });
